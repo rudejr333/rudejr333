7d61608aec20aa48d39368d2dac8b98baeda53c7
rudejr333 <81666706+rudejr333@users.noreply.github.com>
2022-09-19 15:38:57 +0900

    슬라이드 테스트 등등

---
+++ web-3D_slideShow-master/JS/JS.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+});
